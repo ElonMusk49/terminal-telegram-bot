0c8eb670a5abacdc14feb06729a8f73629091a25
ElonMusk49 <101656030+ElonMusk49@users.noreply.github.com>
2022-06-06 23:10:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+from termbot.sample_config import Config
+
+class Development(Config):
+  APP_ID = 13780698
+  API_HASH = "56536f4127ca90a7785d11d2c8e1f031"
+  TG_BOT_TOKEN = "5524167802:AAEPvQ6-JvdwiaIKppweHTYsCXrym7vVHBM"
+  AUTH_USERS = [
+    5455166957
+  ]
